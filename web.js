37e72966532b12d842ae79a7b8dd68c04056653b
rafaeljack3 <rafaeljack3@gmail.com>
2015-03-09 23:01:15 -0300

    modified web.js to read from index.html

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
+
+var text = fs.readFileSync('./index.html','utf8');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('HELLO FUCK WORLD!!!!!!')
+  app.get('/', function(request, response) {
+  response.send(text)
 })
 
 app.listen(app.get('port'), function() {
